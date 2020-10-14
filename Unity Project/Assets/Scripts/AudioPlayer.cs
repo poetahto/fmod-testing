efb7587e375691e4524b2619d61e264528f754b1
poetahto <walls.dk@gmail.com>
2020-10-14 15:42:03 -0700

    removed fmod

---
+++ Unity Project/Assets/Scripts/AudioPlayer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using FMOD.Studio;
+using FMODUnity;
+using UnityEngine;
+
+public class AudioPlayer : MonoBehaviour
+{
+    [SerializeField, EventRef] private string thudSound = default;
+    
+    private bool _playingMusic = false;
+    private float _volume = 1f;
+    private Bus _musicBus;
+
+    private void Start()
+    {
+        _musicBus = RuntimeManager.GetBus("bus:/Master");
+    }
+
+    private void OnGUI()
+    {
+        if (GUILayout.Button("Play thud"))
+            RuntimeManager.PlayOneShot(thudSound);
+
+        if (GUILayout.Toggle(_playingMusic, "Play music") != _playingMusic)
+        {
+            _playingMusic = !_playingMusic;
+            // set music playing to _playingMusic
+        }
+
+        GUILayout.Label("Volume");
+        _volume = GUILayout.HorizontalSlider(_volume, 0f, 1f);
+        _musicBus.setVolume(_volume);
+    }
+}
